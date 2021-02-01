0c4944dc400c8f9abe40de74c1b69d4f060874a2
Chao Xu <mgcclx@gmail.com>
2021-01-31 18:50:33 -0800

    better proof and update katex output

--- math_katex_offline.js
+++ math_katex_offline.js
@@ -34,7 +34,7 @@ for (var i = 0; i < mathElements.length; i++) {
           var texText = mathElements[i].firstChild;
           if (mathElements[i].tagName == "SPAN") {
            katex.render(texText.data, mathElements[i], 
-            { displayMode: mathElements[i].classList.contains("display"), throwOnError: false, macros:macros}
+            { displayMode: mathElements[i].classList.contains("display"), throwOnError: false, macros:macros, output:"html"}
           );
         }
 };
